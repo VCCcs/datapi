f775f162d3bf73fb7627536cd4a63d3e1e594356
vincent.cavaille-coll <vincent.cavaille-coll@contentsquare.com>
2023-10-08 18:26:15 +0200

    style: fix format

--- dataprocessing/computation_test.py
+++ dataprocessing/computation_test.py
@@ -3,8 +3,8 @@ import unittest
 import dataprocessing.computation
 from dataprocessing.entities import AdultEntities
 
-class TestComputation(unittest.TestCase):
 
+class TestComputation(unittest.TestCase):
     dataset = [AdultEntities(1), AdultEntities(2)]
 
     def test_get_average(self):
@@ -14,7 +14,8 @@ class TestComputation(unittest.TestCase):
         self.assertEqual(dataprocessing.computation.get_mean("age", self.dataset), 1.5, "Mean should be 1.5")
 
     def test_get_value_distribution(self):
-        self.assertEqual(dataprocessing.computation.get_value_distribution("age", self.dataset), {1: 1, 2: 1}, "Value distribution should be correct")
+        self.assertEqual(dataprocessing.computation.get_value_distribution("age", self.dataset), {1: 1, 2: 1},
+                         "Value distribution should be correct")
 
 
 if __name__ == '__main__':
