b8fdb3bbe72b2e37c979da9446c754b19264d4e9
vincent.cavaille-coll <vincent.cavaille-coll@contentsquare.com>
2023-10-08 11:37:55 +0200

    fix: docker build command with different folder after infra refacto

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "docker_packaging" {
   provisioner "local-exec" {
     command = <<EOF
 	    aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.eu-west-1.amazonaws.com
-	    docker build --platform linux/amd64 -t "${aws_ecr_repository.alvo-vcc.repository_url}:latest" -f Dockerfile .
+	    docker build --platform linux/amd64 -t "${aws_ecr_repository.alvo-vcc.repository_url}:latest" -f ../Dockerfile ../.
 	    docker push "${aws_ecr_repository.alvo-vcc.repository_url}:latest"
 	    EOF
   }
